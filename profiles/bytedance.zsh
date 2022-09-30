bd26ca546337b79410e0a7112aab7f695314e209
kAzec <kazecx@gmail.com>
2022-10-01 05:13:55 +0800

    update tt aliases

--- profiles/bytedance.zsh
+++ profiles/bytedance.zsh
@@ -8,8 +8,16 @@ alias mdd12p="export MBOX_DEBUG_DEVICE_UDID='00008101-000815663410001E'"
 mdd12p
 
 ## Project aliases
+alias tt='cd ~/Projects/ByteDance/TikTok'
 alias tt1='cd ~/Projects/ByteDance/TikTok1'
 alias tt2='cd ~/Projects/ByteDance/TikTok2'
+alias tt3='cd ~/Projects/ByteDance/TikTok3'
+alias tt4='cd ~/Projects/ByteDance/TikTok4'
+alias tto='open **/TikTokBazel.xcodeproj'
+alias tti='cd Aweme; bundle exec pod install; cd ..'
+alias ttio='cd Aweme; bundle exec pod install; cd ..; open **/TikTokBazel.xcodeproj'
+alias ttbi='bundle install --path Local/bundle && cd Aweme; bundle exec pod install; cd ..'
+alias ttbio='bundle install --path Local/bundle && cd Aweme; bundle exec pod install; cd ..; open **/TikTokBazel.xcodeproj'
 
 ## MBox aliases
 alias mSA='mA ~/Projects/ByteDance/TikTok1 ~/Projects/ByteDance/TikTok2 status'
